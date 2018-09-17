fead1bd9c94ecc066a7ed566948a229980af3fd8
github username <github email address>
2018-09-17 14:30:43 -0400

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -3,11 +3,25 @@ import MovieCard from './card-components/MovieCard.js'
 import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
+  // ✓ renders at least one <MovieCard /> component
+  // ✓ renders 9 <MovieCard /> components
+  // ✓ passes all 4 props to every component
+  // ✓ has generated the correct prop values for the first movie
+  // ✓ has generated the correct prop values for the last movie
 
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
-
+    return movieData.map(function(movieObj){
+     return(
+       <MovieCard
+         key={movieObj.title}
+         title={movieObj.title}
+         IMDBRating={movieObj.IMDBRating}
+         genres={movieObj.genres}
+         poster={movieObj.poster}
+       />
+     )
+   })
+ }
   render() {
     return (
       <div id="movie-showcase">
